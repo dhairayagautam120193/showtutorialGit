6f76ebcdc2b36227b0e217dce772c13598fecf60
Dhairaya Gautam <dhairayagautam1@gmail.com>
2021-02-18 03:20:22 +0530

    message

---
+++ Training2.java
@@ -0,0 +1,21 @@
+package Javaproject.AdvancePractice;
+
+public class Training2 {
+
+
+	public static void main(String[] args) {
+
+
+		int i=1;
+		do {
+
+			System.out.println("Value of i is :"+i);
+
+			i++;
+		}
+		while(i<=5);
+
+
+	}
+
+}
